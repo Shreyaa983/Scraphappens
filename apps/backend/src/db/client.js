1dee5ef4c40707e281bdefe0f8a668ff4d506b23
Shreya Shukla <shreyashukla20042005@gmail.com>
2026-03-13 11:08:22 +0530

    commit-before-deploy

--- apps/backend/src/db/client.js
+++ apps/backend/src/db/client.js
@@ -1,4 +1,20 @@
 import { neon } from "@neondatabase/serverless";
 import { env } from "../config/env.js";
 
-export const sql = neon(env.databaseUrl);
+let cachedClient = null;
+
+function getClient() {
+	if (!env.databaseUrl) {
+		throw new Error("NEON_DATABASE_URL is not configured");
+	}
+
+	if (!cachedClient) {
+		cachedClient = neon(env.databaseUrl);
+	}
+
+	return cachedClient;
+}
+
+export function sql(strings, ...values) {
+	return getClient()(strings, ...values);
+}
